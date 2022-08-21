5cbd3d1f32e72c94f5cf8034a50a46525461b1e2
DivContainer <David@Isecke.de>
2022-08-21 12:21:15 +0200

    Initial Spring Setup

---
+++ src/main/java/de/div/PRPBussgeldrechner/PrpBussgeldrechnerApplication.java
@@ -0,0 +1,13 @@
+package de.div.PRPBussgeldrechner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrpBussgeldrechnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrpBussgeldrechnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/div/PRPBussgeldrechner/PrpBussgeldrechnerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.div.PRPBussgeldrechner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrpBussgeldrechnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
